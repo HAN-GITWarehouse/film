3e38af683fb4f2614f0475f19053bb753c9aabdc
xiaohan <han051298@163.com>
2022-02-23 21:56:12 +0800

    sleep uodata

--- src/components/cinema.jsx
+++ src/components/cinema.jsx
@@ -3,43 +3,49 @@ import axios from "axios"
 
 class Cinema extends Component {
 
-    state={
-      CinemaList:[]
+    state = {
+        CinemaList: [],
+        filmList: []
     }
 
-    componentDidMount(){
+    componentDidMount() {
         axios({
-            method:'GET',
-            url:"https://m.maizuo.com/gateway?cityId=110100&ticketFlag=1&k=7406159",
-            headers:{
-               'x-Client-Info': '{"a" : "300o" , "ch" : "1002", "v" : "5.0.4","e": "16395416565231270166529" , "bc" : "110100"}',
-               'x-Host': 'mall.film-ticket.cinema.list'
+            method: 'GET',
+            url: "https://m.maizuo.com/gateway?cityId=110100&ticketFlag=1&k=7406159",
+            headers: {
+                'x-Client-Info': '{"a" : "300o" , "ch" : "1002", "v" : "5.0.4","e": "16395416565231270166529" , "bc" : "110100"}',
+                'x-Host': 'mall.film-ticket.cinema.list'
             }
-        }).then(res=>{
-            console.log(res.data.data.cinemas);
-            const { data:{data:{cinemas:CinemaList}} }=res || {}
+        }).then(res => {
+            const { data: { data: { cinemas: CinemaList } } } = res || {}
             this.setState({
-                CinemaList
+                CinemaList,
+                filmList: CinemaList
             })
         })
     }
 
+    HandleInout = (event) => {
+        const { filmList } = this.state
+        const newList = filmList.filter(item => item.name.includes(event.target.value));
+        this.setState({
+            CinemaList: newList
+        })
+    }
+
     render() {
-        const { CinemaList }=this.state
+        const { CinemaList } = this.state
         return (
             <div className='Cinema-Container'>
-            {
-            CinemaList.map(item=>{
-                return <dl key={item.cinemaId}>
-                        <dt>{item.name}</dt>
-                        <dd>{item.address}</dd>
-                      </dl>
-            })
-            }
-              <dl>
-                  <dt></dt>
-                  <dd></dd>
-              </dl>
+                <input type="text" style={{ width: '99%', height: '50px', display: 'block' }} onInput={this.HandleInout} />
+                {
+                    CinemaList.map(item => {
+                        return <dl key={item.cinemaId} style={{ border: '1px solid #cecece', padding: '10px' }}>
+                            <dt style={{ fontSize: '20px', lineHeight: '40px' }}>{item.name}</dt>
+                            <dd style={{ fontSize: '14px', lineHeight: '20px' }}>{item.address}</dd>
+                        </dl>
+                    })
+                }
             </div>
         );
     }
